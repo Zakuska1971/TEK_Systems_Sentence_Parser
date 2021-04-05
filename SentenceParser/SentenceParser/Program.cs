949d41d6399ff54a3507fad9dfd4d84df8a391e6
Zakuska1971 <30507046+Zakuska1971@users.noreply.github.com>
2021-04-05 08:31:49 -0600

    Setup Console App
    Setup the console application in VS

---
+++ SentenceParser/SentenceParser/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/*
+ * 
+ * In C#, write a program that parses a sentence and replaces each word with
+ * the following:
+ *
+ * first letter, number of distinct characters between first and last character, 
+ * and last letter.  For example, Smooth would become S3h.  Words are separated
+ * by spaces or non-alphabetic characters and these separators should be maintained
+ * in their original form and location in the answer.
+ * 
+ * The code must be syntactically correct and build in visual studio, either as a
+ * console or winforms application.
+ * 
+ * Please paste these instructions at the top of your completed assignment.
+ *
+*/
+
+namespace SentenceParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
